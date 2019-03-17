4d15f4caf097e137a5273f3da45adb6820974db5
Your Name <you@example.com>
2019-03-16 23:52:53 -0700

    generating random title and updating README

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+const { randomTitles } = require('../data/randomTitles')
+
+Header.propTypes = {
+  randomTitles: PropTypes.array,
+}
 
 export default function Header() {
+  const selectedIndex = Math.floor(Math.random() * Math.floor(randomTitles.length))
   return (
-    <div>header!</div>
+    <div>
+      {randomTitles[selectedIndex]}
+    </div >
   )
 }
---
+++ src/data/randomTitles.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "randomTitles": [
+    'Five Seconds A Shark',
+    'Next Shark Game',
+    'Next Shark Game: Barkfest',
+    'Sharky Clicker',
+    'Weird Oceans',
+    'You Have To Name The Shark Game',
+    'Shark A Lark',
+    'Bark Shark',
+    'Fin Idle',
+    'Ray of Dreams',
+    'Shark Saver',
+    'Shoal Sharker',
+    'Shark Souls',
+    'Saucy Sharks',
+    'Sharkfall',
+    'Heart of Sharkness',
+    'Sharks and Recreation',
+    'Alone in the Shark',
+    'Sharkpocalypse',
+    'Shark of Darkness',
+    'Strange Oceans'
+  ]
+}
